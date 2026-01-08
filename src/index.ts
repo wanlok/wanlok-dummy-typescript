58446a304334e2c53e39d91c5535ff1b255ddb00
Robert Wan <wantunglok@gmail.com>
2026-01-08 23:06:55 +1100

    list and delete macos metadata files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { Dummy } from "./logic/Dummy";
+import { FileUtils } from "./logic/FileUtils";
 
 const main = async () => {
   // Dummy.download("https://www.google.com/search?q=1+AUD+to+HKD", "");
-  console.log("Hello World");
+  const command = process.argv[2];
+  const directoryPath = process.argv[3];
+  if (command === "list") {
+    if (FileUtils.isDirectoryPath(directoryPath)) {
+      FileUtils.listMacOSMetadataFiles(directoryPath);
+    }
+  } else if (command === "delete") {
+    if (FileUtils.isDirectoryPath(directoryPath)) {
+      await FileUtils.deleteMacOSMetadataFiles(directoryPath);
+    }
+  }
 };
 
 main();
--- src/logic/FileUtils.ts
+++ src/logic/FileUtils.ts
@@ -1,11 +1,18 @@
 import * as fs from "fs";
 import * as path from "path";
 
+const isDirectoryPath = (directoryPath?: string): boolean => {
+  if (!directoryPath || directoryPath.length === 0) {
+    return false;
+  }
+  return fs.statSync(directoryPath).isDirectory();
+};
+
 const listDirectoryPath = (directoryPath: string, listDirectory: boolean) => {
   let filePaths: string[] = [];
   fs.readdirSync(directoryPath).forEach((file) => {
     const filePath = path.join(directoryPath, file);
-    const isDirectory = fs.statSync(filePath).isDirectory();
+    const isDirectory = isDirectoryPath(filePath);
     if ((listDirectory && isDirectory) || !isDirectory) {
       filePaths.push(filePath);
     }
@@ -16,17 +23,32 @@ const listDirectoryPath = (directoryPath: string, listDirectory: boolean) => {
   return filePaths;
 };
 
+const isMacOSMetadataFile = (filePath: string): boolean => {
+  const fileName = filePath.substring(filePath.lastIndexOf(path.sep) + 1);
+  return fileName === ".DS_Store" || fileName.startsWith("._");
+};
+
 const listMacOSMetadataFiles = (directoryPath: string) => {
   const filePaths = FileUtils.listDirectoryPath(directoryPath, false);
+  let count = 0;
   for (const filePath of filePaths) {
-    const fileName = filePath.substring(filePath.lastIndexOf(path.sep) + 1);
-    if (fileName === ".DS_Store" || fileName.startsWith("._")) {
+    if (isMacOSMetadataFile(filePath)) {
+      count = count + 1;
       console.log(filePath);
     }
   }
+  console.log("COUNT: " + count);
+};
+
+const deleteMacOSMetadataFiles = async (directoryPath: string) => {
+  const filePaths = FileUtils.listDirectoryPath(directoryPath, false);
+  const macOSMetadataFiles = filePaths.filter(isMacOSMetadataFile);
+  await (await import("trash")).default(macOSMetadataFiles);
 };
 
 export const FileUtils = {
+  isDirectoryPath,
   listDirectoryPath,
-  listMacOSMetadataFiles
+  listMacOSMetadataFiles,
+  deleteMacOSMetadataFiles
 };
