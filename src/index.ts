56332d4e3dd1e18b9b77c95b9672d59be083ac27
Robert Wan <wantunglok@gmail.com>
2026-01-25 20:28:52 +1100

    add execute web task function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
+import { centralfieldWebPageTask } from "./task/CentralfieldWebPageTask";
+import { jumboComputerWebPageTask } from "./task/JumboComputerWebPageTask";
 import { CurrencyUtils } from "./utilities/CurrencyUtils";
 import { FileUtils } from "./utilities/FileUtils";
 
+const webPageTasks = [centralfieldWebPageTask, jumboComputerWebPageTask];
+
+const executeWebPageTask = async (inputs: string[]) => {
+  for (const task of webPageTasks) {
+    if (task.isResponsibleFor(inputs)) {
+      let count = 0;
+      const content = [];
+      const urlStrings = await task.getPaginationUrlStrings(inputs);
+      for (const urlString of urlStrings) {
+        content.push(...(await task.getContent(urlString)));
+        if (count === 1) {
+          break;
+        }
+        count = count + 1;
+      }
+      console.log(content);
+    }
+  }
+};
+
 const main = async () => {
-  const command = process.argv[2];
+  const inputs = process.argv.slice(2);
+  const [command] = inputs;
   if (command === "list") {
     const directoryPath = process.argv[3];
     if (FileUtils.isDirectoryPath(directoryPath)) {
@@ -17,6 +40,8 @@ const main = async () => {
     const from = process.argv[3];
     const to = process.argv[4];
     CurrencyUtils.printExchangeRates(from, to);
+  } else {
+    await executeWebPageTask(inputs);
   }
 };
 
