73bb1b99c8f137a35a59af75e263fdfc99203cfe
Robert Wan <wantunglok@gmail.com>
2026-09-04 23:55:42 +1000

    add filter by substring

--- src/utilities/getHtmlAHrefsByClassName.ts
+++ src/utilities/getHtmlAHrefsByClassName.ts
@@ -1,18 +1,18 @@
 import { JSDOM } from "jsdom";
 
-export const getAHrefs = (as: HTMLCollectionOf<Element>) => {
+export const getAHrefs = (as: HTMLCollectionOf<Element>, substring?: string) => {
   const hrefs = new Set<string>();
   Array.from(as).forEach((a: Element) => {
     const href = a.getAttribute("href");
-    if (href) {
+    if (href && (!substring || href.includes(substring))) {
       hrefs.add(href);
     }
   });
   return Array.from(hrefs);
 };
 
-export const getHtmlAHrefsByClassName = (html: string, className: string): string[] => {
+export const getHtmlAHrefsByClassName = (html: string, className: string, substring?: string): string[] => {
   const dom = new JSDOM(html);
   const as = dom.window.document.getElementsByClassName(className);
-  return getAHrefs(as);
+  return getAHrefs(as, substring);
 };
--- src/utilities/getHtmlAHrefsInClassName.ts
+++ src/utilities/getHtmlAHrefsInClassName.ts
@@ -1,13 +1,13 @@
 import { JSDOM } from "jsdom";
 import { getAHrefs } from "./getHtmlAHrefsByClassName";
 
-export const getHtmlAHrefsInClassName = (html: string, className: string): string[] => {
+export const getHtmlAHrefsInClassName = (html: string, className: string, substring?: string): string[] => {
   const dom = new JSDOM(html);
   const containers = dom.window.document.getElementsByClassName(className);
   const hrefs = new Set<string>();
   Array.from(containers).forEach((container) => {
     const as = container.getElementsByTagName("a");
-    getAHrefs(as).forEach((href) => hrefs.add(href));
+    getAHrefs(as, substring).forEach((href) => hrefs.add(href));
   });
   return Array.from(hrefs);
 };
